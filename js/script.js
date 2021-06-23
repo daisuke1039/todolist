25f3b1db670df33929a831a60303197aca71124f
hattori daisuke <h.daisuke1039@gmail.com>
2021-06-23 16:43:50 +0900

    Improve processing for arrays

--- js/script.js
+++ js/script.js
@@ -5,98 +5,85 @@
   const $menuItem = $menu.querySelectorAll('[data-menu]');
   const $content = document.querySelectorAll('[data-content]');
 
-  const menuClick = (e) => {
+  const clickTabMenu = (e) => {
     e.preventDefault();
 
     const $this =  e.target;
     const dataTarget = $this.dataset.menu;
 
-    let index = 0;
-    while( index < $menuItem.length ) {
-    $content[index].classList.remove('is-show');
-    $menuItem[index].classList.remove('is-choice');
-    index++;
-    };
+    $menuItem.forEach((item, index) => {
+      $content[index].classList.remove('is-show');
+      item.classList.remove('is-choice');
+    });
   
     $content[dataTarget].classList.add('is-show');
     $this.classList.add('is-choice');
-
   };
 
-  let index = 0;
-  while( index < $menuItem.length ) {
-    $menuItem[index].addEventListener('click' , (e) => menuClick(e));
-    index++;
-  };
+  $menuItem.forEach(item => {
+    item.addEventListener('click' , (e) => clickTabMenu(e));
+  });
 
   //TODOLISTの機能
   const $addBtn = document.querySelector('.add_btn');
 
   $addBtn.addEventListener('click'　, () => {
-
     const $addInput = document.querySelector('input');
-
     if( $addInput.value !== "" ) {
 
       //要素を追加
       const $taskBody = document.createElement('li');
       const $taskText = document.createElement('p');
-      const $taskCheck = document.createElement('button');
-      const $taskDelete = document.createElement('button');
+      const $taskCheckBtn = document.createElement('button');
+      const $taskDeleteBtn = document.createElement('button');
 
       //追加した要素のclass名をつける
       $taskBody.className = 'task_item';
-      $taskCheck.className = 'check_btn';
-      $taskDelete.className = 'delete_btn';
+      $taskCheckBtn.className = 'check_btn';
+      $taskDeleteBtn.className = 'delete_btn';
 
       //addInputで入力された文字をtaskTextのテキストに渡す
       $taskText.textContent = $addInput.value;
       $addInput.value = "";
 
-      const $taskUl = document.querySelector('.js_task');
-
       //追加した要素の位置を指定
+      const $taskUl = document.querySelector('.js_task');
       $taskUl.appendChild($taskBody);
       $taskBody.appendChild($taskText);
-      $taskBody.appendChild($taskCheck);
-      $taskBody.appendChild($taskDelete);   
+      $taskBody.appendChild($taskCheckBtn);
+      $taskBody.appendChild($taskDeleteBtn);   
 
-      //完了アクション
-      //追加された要素のCHECKボタンの処理
-      $taskCheck.addEventListener('click', () => {
-        const cItem = $taskCheck.closest('.task_item');
+      //完了アクション : 追加された要素のCHECKボタンの処理
+      $taskCheckBtn.addEventListener('click', () => {
+        const parentCheckItem = $taskCheckBtn.closest('.task_item');
         const $endUl = document.querySelector('.js_end');
-        $taskCheck.remove();
-        cItem.classList.add('end_item');
-        $endUl.insertBefore(cItem , $endUl.firstChild);
+        $taskCheckBtn.remove();
+        parentCheckItem.classList.add('end_item');
+        $endUl.insertBefore(parentCheckItem , $endUl.firstChild);
       });
       
-      //削除アクション
-      //追加された要素のDELETEボタンの処理
-      $taskDelete.addEventListener('click', () => {
-        const dItem = $taskDelete.closest('.task_item');
-        dItem.remove();
+      //削除アクション : 追加された要素のDELETEボタンの処理
+      $taskDeleteBtn.addEventListener('click', () => {
+        const parentDeleteItem = $taskDeleteBtn.closest('.task_item');
+        parentDeleteItem.remove();
       });
 
     };
-
   });
 
   //日付の表示
-  const nowDate =  new Date();
-  const year = nowDate.getFullYear();
-  const months = nowDate.getMonth() + 1;
-  const date = nowDate.getDate();
-  const days = nowDate.getDay();
-
   const getDate = () => {
+    const nowDate =  new Date();
+    const year = nowDate.getFullYear();
+    const months = nowDate.getMonth() + 1;
+    const date = nowDate.getDate();
+    const days = nowDate.getDay();
 
-   const month = ["","January","February","March","April","May","June","July","August","September","October","November","December"];
-   const day = ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
+    const month = ["","January","February","March","April","May","June","July","August","September","October","November","December"];
+    const day = ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
 
-   const t = `${month[months]} / ${year} . ${months} . ${date} (${day[days]})`;
-   return t;
-
+    const t = `${month[months]} / ${year} . ${months} . ${date} (${day[days]})`;
+    return t;
   };
 
   const $ampm = document.querySelector('.ampm');
@@ -137,11 +124,10 @@
   repeOutputTime();
   
   const $dateText = document.querySelectorAll('.date_text');
-  let dateNum = 0;
-  while( dateNum < $dateText.length ) { 
-    $dateText[dateNum].textContent = getDate();
-    dateNum++;
-  };
+  
+  $dateText.forEach(item => {
+    item.textContent = getDate();
+  })
 
   const $closeBtn = document.querySelector('.m_close');
   const $openBtn = document.querySelector('.m_open');
